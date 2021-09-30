b144e8f37679b23980120eaafe8670af30e0b253
Christoforos <christoforos@christoforos.dev>
2021-09-30 13:25:19 +0300

    ios fixes

--- src/index.ios.tsx
+++ src/index.ios.tsx
@@ -12,54 +12,98 @@ type NeumorphismProps = {
 };
 
 export default function Neumorphism(props: NeumorphismProps) {
-  let shadowProps = {
-    shadowRadius: 3,
-    shadowOffset: 5,
-    shadowOpacity: 0.5,
-    inset: props.shapeType === 'pressed',
-  };
-  return (
-    <View style={[styles.container, props.style]}>
-      {props.children}
-      {!props.disabled && (
-        <>
-          <Shadow
-            type={'left'}
-            {...shadowProps}
-            shadowColor={props.lightColor}
-          />
-          <Shadow
-            type={'top'}
-            {...shadowProps}
-            shadowColor={props.lightColor}
-          />
-          <Shadow
-            type={'right'}
-            {...shadowProps}
-            shadowColor={props.darkColor}
-          />
-          <Shadow
-            type={'bottom'}
-            {...shadowProps}
-            shadowColor={props.darkColor}
-          />
-        </>
-      )}
-    </View>
-  );
+  if (props.shapeType === 'pressed') {
+    let shadowProps = {
+      shadowRadius: 3,
+      shadowOffset: 5,
+      shadowOpacity: 1,
+    };
+    return (
+      <View
+        style={[
+          insetStyles.container,
+          { borderRadius: props.radius },
+          props.style,
+        ]}
+      >
+        {props.children}
+        {!props.disabled && (
+          <>
+            <Shadow
+              type={'left'}
+              {...shadowProps}
+              shadowColor={props.darkColor}
+            />
+            <Shadow
+              type={'top'}
+              {...shadowProps}
+              shadowColor={props.darkColor}
+            />
+            <Shadow
+              type={'right'}
+              {...shadowProps}
+              shadowColor={props.lightColor}
+            />
+            <Shadow
+              type={'bottom'}
+              {...shadowProps}
+              shadowColor={props.lightColor}
+            />
+          </>
+        )}
+      </View>
+    );
+  } else {
+    return (
+      <View
+        style={{
+          shadowColor: props.lightColor,
+          shadowOffset: {
+            width: -5,
+            height: -5,
+          },
+          shadowOpacity: 1,
+          shadowRadius: 3,
+        }}
+      >
+        <View
+          style={{
+            shadowColor: props.darkColor,
+            shadowOffset: {
+              width: 5,
+              height: 5,
+            },
+            shadowOpacity: 1,
+            shadowRadius: 3,
+          }}
+        >
+          <View>{props.children}</View>
+        </View>
+      </View>
+    );
+  }
 }
 
 const Shadow = ({
   type,
-  inset,
   shadowColor,
   shadowOpacity,
   shadowOffset,
   shadowRadius,
 }: ShadowProps) => {
+  if (shadowOffset === undefined) {
+    shadowOffset = 5;
+  }
+
+  let offsets = {
+    left: shadowOffset,
+    top: shadowOffset,
+    right: -shadowOffset,
+    bottom: -shadowOffset,
+  };
   const offsetStyle = {
-    width: ['left', 'right'].includes(type) ? shadowOffset || 5 : 0,
-    height: ['top', 'bottom'].includes(type) ? shadowOffset || 5 : 0,
+    width: ['left', 'right'].includes(type) ? offsets[type] : 0,
+    height: ['top', 'bottom'].includes(type) ? offsets[type] : 0,
   };
 
   const shadowStyle = {
@@ -68,16 +112,7 @@ const Shadow = ({
     shadowRadius: shadowRadius,
     shadowOpacity: shadowOpacity,
   };
-  return (
-    <View
-      style={[
-        styles.shadow,
-        styles[type],
-        { [type]: inset ? -10 : 0 },
-        shadowStyle,
-      ]}
-    />
-  );
+  return <View style={[insetStyles.shadow, insetStyles[type], shadowStyle]} />;
 };
 
 interface ShadowProps {
@@ -86,13 +121,11 @@ interface ShadowProps {
   shadowOpacity?: number;
   shadowOffset?: number;
   shadowRadius?: number;
-  inset?: boolean;
 }
 
-const styles = StyleSheet.create({
+const insetStyles = StyleSheet.create({
   container: {
     overflow: 'hidden',
-    height: '100%',
   },
   shadow: {
     position: 'absolute',
@@ -102,17 +135,21 @@ const styles = StyleSheet.create({
   left: {
     width: 10,
     height: '100%',
+    left: -10,
   },
   top: {
     height: 10,
     width: '100%',
+    top: -10,
   },
   right: {
     width: 10,
     height: '100%',
+    right: -10,
   },
   bottom: {
     height: 10,
     width: '100%',
+    bottom: -10,
   },
 });
